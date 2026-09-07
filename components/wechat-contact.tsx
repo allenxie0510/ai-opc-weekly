41f071cd4dfb126cbf1bf28f9eef406d9c176d0a
allenxie0510 <147039871+allenxie0510@users.noreply.github.com>
2026-09-07 17:19:53 +0800

    style: center WeChat caption and match footer slogan size

--- components/wechat-contact.tsx
+++ components/wechat-contact.tsx
@@ -5,6 +5,6 @@ export function WechatContact() {
     <a href="/wechat-contact.jpg" target="_blank" rel="noopener noreferrer" aria-label="打开管理员微信二维码大图">
       <Image src="/wechat-contact.jpg" alt="管理员微信二维码，扫码添加微信进行反馈或合作咨询" width={653} height={644} unoptimized />
     </a>
-    <figcaption><span>扫描二维码，</span><span>添加管理员微信</span></figcaption>
+    <figcaption>微信联系我</figcaption>
   </figure>;
 }
